0c396ee8072cc56c1b30dda31639d59b72c9482f
Floppy <212432561+Floppy8@users.noreply.github.com>
2026-04-25 21:11:58 -0500

    UI and CoLoRs Update!

--- src/main.py
+++ src/main.py
@@ -3,7 +3,10 @@
 
     Made By: PseudoSinusoidal
 
-    Started: 2025-08-09
+    Started: 2025-08-09  (YYYY-MM-DD)???
+
+    Edit Log:
+        Floppy 2026-04-25: UI and CoLoRs Update!
 '''
 from C555 import Calc555 # 555 IC Timer Calculator
 from OLaw import OLawBase # Ohm's Law Calculator
@@ -17,16 +20,44 @@ modules = {
     "3": RCalc
 }
 
-mm = "[*] Electrical Calculators by Pseudosinusoidal\n[!] Enter the corresponding number to use module.\n[-1] Exit\n[1] 555 Timer IC\n[2] Ohm's Law\n[3] Resistor Calculator"
+# ANSI Colors
+RED = "\x1b[31m"
+GREEN = "\x1b[32m"
+YELLOW = "\x1b[33m"
+BLUE = "\x1b[34m"
+PURPLE = "\x1b[35m"
+CYAN = "\x1b[36m"
+RESET = "\x1b[0m"
+
+mm = rf"""
+=============================================================
+   ____ _                _ _   ____        _
+  / ___(_)_ __ ___ _   _(_) |_/ ___|  ___ | |_   _____ _ __
+ | |   | | '__/ __| | | | | __\___ \ / _ \| \ \ / / _ \ '__|
+ | |___| | | | (__| |_| | | |_ ___) | (_) | |\ V /  __/ |
+  \____|_|_|  \___|\__,_|_|\__|____/ \___/|_| \_/ \___|_|
+
+=============================================================
+      {GREEN}Developed by PseudoSinusoidal      {YELLOW}Version: 1.0.0{RESET}
+-------------------------------------------------------------
+ {PURPLE}Please Select from the following choices:{RESET}
+
+ [1] {YELLOW}555 Timer IC{RESET}
+ [2] {YELLOW}Ohm's Law{RESET}
+ [3] {YELLOW}Resistor Utilities{RESET}
+
+ [CTRL+C] {RED}Exit{RESET}
+-------------------------------------------------------------"""
 
 show_menu(mm)
-while True:
-    ms = input("> ")
-    if ms == "-1":
-        print("[!] Terminating program..")
-        exit()
-    elif ms in modules: # Find module number
-        modules[ms]()
-        show_menu(mm)  # Show the menu again after module execution
-    else:
-        print("[!] Invalid value. Try again.")
+try:
+    while True:
+        ms = input(" > ")
+        if ms in modules: # Find module number
+            modules[ms]()
+            show_menu(mm)  # Show the menu again after module execution
+        else:
+            print("[!] Invalid value. Please try again.")
+        pass
+except KeyboardInterrupt:
+    print("[!] Exiting Program...")
