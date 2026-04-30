269052342fa0b06475e556d81917b2bde17eb2f8
PseudoSinusoidal <zra7234@gmail.com>
2026-04-30 17:12:43 -0400

    Update import statements to use modules directory

--- src/circuitsolver.py
+++ src/circuitsolver.py
@@ -6,12 +6,12 @@
     Started: 2025-08-09  (YYYY-MM-DD)
 '''
 
-from C555 import *
-from OLaw import * 
-from RCalc import * 
-from BJTFET import *
-from IntCirc import *
-from CalcUtilities import *
+from modules.C555 import *
+from modules.OLaw import * 
+from modules.RCalc import * 
+from modules.BJTFET import *
+from modules.IntCirc import *
+from modules.Utilities import *
 
 # Module Dictionary
 modules = {
