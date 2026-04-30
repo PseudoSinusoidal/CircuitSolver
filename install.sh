3c0f393ae843ab4e0a44d06bd67cde987a0faa55
Floppy <212432561+Floppy8@users.noreply.github.com>
2026-04-30 11:01:36 -0500

    Change installation branch from cli-tester to cli
    Updated installation script to download the 'cli' branch instead of 'cli-tester'.

--- install.sh
+++ install.sh
@@ -1,17 +1,17 @@
 #!/usr/bin/env bash
 set -e
 
-echo "Installing CircuitSolver (cli-tester)..."
+echo "Installing CircuitSolver..."
 
 # clean old install
 rm -rf "$HOME/CircuitSolver-cli"
 
 # download correct branch
-curl -fsSL https://github.com/PseudoSinusoidal/CircuitSolver/archive/refs/heads/cli-tester.tar.gz | tar -xz -C "$HOME"
+curl -fsSL https://github.com/PseudoSinusoidal/CircuitSolver/archive/refs/heads/cli.tar.gz | tar -xz -C "$HOME"
 
 # rename extracted folder to stable name
 rm -rf "$HOME/CircuitSolver-cli"
-mv "$HOME/CircuitSolver-cli-tester" "$HOME/CircuitSolver-cli"
+mv "$HOME/CircuitSolver-cli" "$HOME/CircuitSolver-cli"
 
 # create command
 mkdir -p "$HOME/.local/bin"
