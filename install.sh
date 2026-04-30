31ffe3d6334aa1313a015ac032ac0e61b1414d7b
Floppy <212432561+Floppy8@users.noreply.github.com>
2026-04-30 10:09:37 -0500

    Update installation script for CircuitSolver tester

--- install.sh
+++ install.sh
@@ -4,16 +4,16 @@ set -e
 echo "Installing CircuitSolver..."
 
 # remove old version
-rm -rf "$HOME/CircuitSolver-cli"
+rm -rf "$HOME/CircuitSolver-cli-tester"
 
 # download + extract
-curl -fsSL https://github.com/PseudoSinusoidal/CircuitSolver/archive/refs/heads/cli.tar.gz | tar -xz -C "$HOME"
+curl -fsSL https://github.com/PseudoSinusoidal/CircuitSolver/archive/refs/heads/cli-tester.tar.gz | tar -xz -C "$HOME"
 
 # create command
 mkdir -p "$HOME/.local/bin"
 
 echo '#!/usr/bin/env bash
-cd "$HOME/CircuitSolver-cli"
+cd "$HOME/CircuitSolver-cli-tester"
 python3 -m circuitsolver.main "$@"' > "$HOME/.local/bin/circuitsolver"
 
 chmod +x "$HOME/.local/bin/circuitsolver"
