d8019e0970bae0cf3de43f33661fc2933cb6319f
Z.R. <zra7234@gmail.com>
2026-05-01 19:29:47 -0400

    Add BJTFET function with menu and placeholders
    Implemented initial structure for BJTFET function with menu options for BJT and MOSFET calculations.

--- src/modules/BJTFET.py
+++ src/modules/BJTFET.py
@@ -1,4 +1,31 @@
+# BJT & MOSFET Calculations Module
+# By Pseudosinusoidal
 from modules.Utilities import *
 
 def BJTFET():
-    pass # WIP
+    formulas = {
+        "BJTBase": {
+            "placeheld": lambda example: round(),
+        },
+        "FETGate": {
+            "placeheld": lambda example: round()
+        }
+    }
+
+    mm = f"{PURPLE} [*] BJT & MOSFET Calculator{RESET}\n\n   [1] {YELLOW}BJT Base Resistor{RESET} \n   [2] {YELLOW}MOSFET Gate Series Resistor{RESET}\n\n [CTRL+C] {RED}Back{RESET}\n"
+    show_menu(mm)
+    
+    try:
+        while True:
+            ms = input(" > ")
+            
+            if ms == "1":
+                clear()
+
+            if ms == "2":
+                clear()
+                
+            else:
+                print(invalid)
+    except KeyboardInterrupt:
+        print(terminate)
