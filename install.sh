b25dc74e05a2e660bc59bb81b0ccad4ec6224350
Floppy <212432561+Floppy8@users.noreply.github.com>
2026-05-02 18:59:42 -0500

    install.sh update

--- install.sh
+++ install.sh
@@ -6,19 +6,31 @@ set -e
 # Install Location
 INSTALL_DIR="$HOME/.local/share/circuitsolver"
 
+# Check if there's an old install
 if [[ -d "$INSTALL_DIR" ]]; then
+	# Ask installer
 	echo "[!] CircuitSolver was found on your device. Would you like to replace the old one?"
 	echo "[!] Warning: Not removing the previous version can cause unexpected behaviors!"
-	read -p "[?] Replace? (Y/N) " install_answer
+	read -p "[?] Replace? (Y/N) " install_answer < /dev/tty
 
 	if [[ "$install_answer" == "Y" ]]; then
 		rm -rf "$INSTALL_DIR" # Remove old directory
-		mkdir -p "$INSTALL_DIR" # Add new directory
+
 	elif [[ "$install_answer" == "N" ]]; then
-		mkdir -p "$INSTALL_DIR" # Add new directory
+
+	# Invalid
 	else
-		echo "[!] Invalid answer, please use "Y" or "N". "
+		echo '[!] Invalid answer, please retry.'
+		exit 1
+
 	fi
+fi
+
+mkdir -p "$INSTALL_DIR" # Add new directory
+
+# Remove any old install temp stuff
+rm -f /tmp/cs.tar.gz
+rm -rf /tmp/CircuitSolver-*
 
 # Download the repo tarball to /tmp
 curl -fsSL https://github.com/PseudoSinusoidal/CircuitSolver/archive/refs/heads/cli.tar.gz -o /tmp/cs.tar.gz
