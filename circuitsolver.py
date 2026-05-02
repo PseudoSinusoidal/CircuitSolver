528f4247d8e2bc94f9a51415dd67dfbef30e4445
Floppy <212432561+Floppy8@users.noreply.github.com>
2026-05-01 19:29:39 -0500

    Updated title, icon, and updated readme.

---
+++ circuitsolver.py
@@ -0,0 +1,60 @@
+''' 
+    Title: CircuitSolver CLI
+
+    Made By: PseudoSinusoidal & Floppy
+
+    Started: 2025-08-09  (YYYY-MM-DD)
+'''
+
+from modules.OLaw import * 
+from modules.RCalc import * 
+from modules.BJTFET import *
+from modules.IntCirc import *
+from modules.Utilities import *
+
+# Module Dictionary
+modules = {
+    "1": Calc555,
+    "2": OLawBase,
+    "3": RCalc,
+    "4": BJTFET,
+    "5": IntCirc
+}
+
+mm = rf"""
+============================================================={YELLOW}
+                  ,/      ___ _             _ _
+                ,'/      / __(_)_ _ __ _  _(_) |_
+              ,' /      | (__| | '_/ _| || | |  _|
+            ,'  /_____   \___|_|_| \__|\_,_|_|\__|
+          .'____    ,'    ___      _
+               /  ,'     / __| ___| |_ _____ _ _
+              / ,'       \__ \/ _ \ \ V / -_) '_|
+             /,'         |___/\___/_|\_/\___|_|
+            /'
+{RESET}=============================================================
+      {GREEN}Developed by PseudoSinusoidal      {YELLOW}Version: SOURCE{RESET}
+-------------------------------------------------------------
+ {PURPLE}Please select from the following choices:{RESET}
+
+ [1] {YELLOW}555 Timer IC{RESET}
+ [2] {YELLOW}Ohm's Law{RESET}
+ [3] {YELLOW}Resistor Utilities{RESET}
+ [4] {YELLOW}BJT & MOSFET Utilities{RESET}
+ [5] {YELLOW}Integrated Circuit Utilities{RESET}
+
+ [CTRL+C] {RED}Exit{RESET}
+-------------------------------------------------------------"""
+
+show_menu(mm)
+try:
+    while True:
+        ms = input(" > ")
+        if ms in modules: # Find module number
+            modules[ms]()
+            show_menu(mm)  # Show the menu again after module execution
+        else:
+            print(invalid)
+        pass
+except KeyboardInterrupt:
+    print(f"\n{RED}[!] Exiting Program...{RESET}")
